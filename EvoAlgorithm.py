3f0488036846d3382d2359580961b1aaf7832406
Brandon Cuza <brandon.h.cuza@gmail.com>
2023-05-04 14:28:13 -0230

    Fully implemented cycle crossover function

--- EvoAlgorithm.py
+++ EvoAlgorithm.py
@@ -6,42 +6,67 @@ numPop = 1000
 numGen = 1000
 
 # Function that takes two parents and returns two children created through
-# cycle crossover. !Incomplete!
+# cycle crossover.               
 def cycleCross(parent1, parent2):
+    parent1 = parent1.getNodes()
+    parent2 = parent2.getNodes()
+
+    columns = columnize(parent1, parent2)
+
     cycles = []
-    p1 = parent1.getNodes().copy()
-    for node in p1:
-        print(node.getName())
-    print("\n")
-    p2 = parent2.getNodes().copy()
-    for node in p2:
-        print(node.getName())
-    print("\n")
-    while True:
-        cycle = []
-        i = 0
-        cycle.append(p1.pop(i))
-        while True:
-            nextNum = p2[i]
-            try:
-                temp = p1.index(nextNum)
-                cycle.append(p2.pop(i))
-                i = temp
-                p1.pop(i)
-            except ValueError:
-                p2.pop(i)
-                cycles.append(cycle)
-                break
-        if len(p1) == 0:
-            break
+    for column in columns:
+
+        # Check if the column is already in a cycle
+        skip = False
+        for cycle in cycles:
+            if column in cycle:
+                skip = True
+
+        # If not, it is the head of a new cycle     
+        if not skip:
+            head = column[1]
+            nextType = column[2]
+            cycle = [column]
+            # Find columns in this cycle, end when the next type is the same as the head
+            while nextType != head:
+                for otherColumn in columns:
+                    if otherColumn[1] == nextType:
+                        cycle.append(otherColumn)
+                        nextType = otherColumn[2]
+            cycles.append(cycle)
+
+    # Construct children
+    child1 = [None]*18
+    child2 = [None]*18
     for cycle in cycles:
-        for node in cycle:
-            print(node.getName())
-        print("\n")
-                
-            
-        
-        
+        if (cycles.index(cycle) % 2 == 0):
+            for column in cycle:
+                child1[column[0]] = column[1]
+                child2[column[0]] = column[2]
+        else:
+            for column in cycle:
+                child1[column[0]] = column[2]
+                child2[column[0]] = column[1]
+    return child1, child2 
+
+# Function that takes two lists and creates columns that contain the index value,
+# and the value of the first and second list at that index (i.e. [3,4,5] and [6,7,8]
+# create the columns [0,3,6], [1,4,7], and [2,5,8]). This is useful for cycle crossover.
+def columnize(list1, list2):
+    columns = []
+    for node in list1:
+        column = []
+        index = list1.index(node)
+        column.append(index)
+        column.append(list1[index])
+        column.append(list2[index])
+        columns.append(column)
+    return columns
+
+# Function that selects parents from the population by roulette wheel (fitness proportionate)
+# selection. !Incomplete!
+def rouletteSelect():
+    break
 
 # Create nodes and edges (I intend to replace this with something
 # more generic in the future)
